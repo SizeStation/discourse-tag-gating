fa35c35bae07d0989d5e5809db2717661c2ce225
Skydev125 <diogo_goncalves2004@hotmail.com>
2026-02-18 23:21:20 +0000

    refactor: simplify and generalize logic

--- plugin.rb
+++ plugin.rb
@@ -15,19 +15,25 @@ module ::MyPluginModule
 end
 
 module ::DiscourseTagGating
-  def self.nsfw_access?(user)
+  def self.has_access?(user)
     return false if user.blank?
     return true if user.staff?
     expected = SiteSetting.tag_gating_user_field_logic ? "true" : "false"
     user.user_fields[SiteSetting.tag_gating_user_field_id.to_s] == expected
   end
 
-  def self.topic_has_nsfw_tag?(topic)
+  def self.topic_has_tag?(topic)
     if topic.association(:tags).loaded?
       topic.tags.map(&:name)
     else
       topic.tags.pluck(:name)
-    end.include?("nsfw")
+    end.include?(SiteSetting.tag_gating_tag_name)
+  end
+
+  def self.blocked_topic_ids_for(user)
+    tag_id = Tag.where(name: SiteSetting.tag_gating_tag_name).select(:id)
+    topic_ids = TopicTag.where(tag_id: tag_id).select(:topic_id)
+    Topic.where(id: topic_ids).where.not(user_id: user&.id).select(:id)
   end
 end
 
@@ -41,11 +47,11 @@ after_initialize do
       return false unless super
       return true if topic.user_id == user&.id
 
-      if DiscourseTagGating.topic_has_nsfw_tag?(topic) && !DiscourseTagGating.nsfw_access?(user)
+      if DiscourseTagGating.topic_has_tag?(topic) && !DiscourseTagGating.has_access?(user)
         raise Discourse::InvalidAccess.new(
-                "nsfw_access_required",
+                "access_required",
                 topic,
-                custom_message: "discourse_tag_gating.nsfw_access_required",
+                custom_message: "discourse_tag_gating.access_required",
               )
       end
       true
@@ -53,17 +59,14 @@ after_initialize do
   end
 
   # --- 2. THE FILTER (Post Scope) ---
-  module FilterNSFW
+  module FilterGatedPosts
     def secured(guardian = nil, *args, **kwargs)
       results = super
       return results unless SiteSetting.tag_gating_enabled
       user = guardian.respond_to?(:user) ? guardian.user : nil
 
-      unless DiscourseTagGating.nsfw_access?(user)
-        nsfw_tag_id = Tag.where(name: "nsfw").select(:id)
-        nsfw_topic_ids = TopicTag.where(tag_id: nsfw_tag_id).select(:topic_id)
-        blocked_topic_ids = Topic.where(id: nsfw_topic_ids).where.not(user_id: user&.id).select(:id)
-        results = results.where.not(topic_id: blocked_topic_ids)
+      unless DiscourseTagGating.has_access?(user)
+        results = results.where.not(topic_id: DiscourseTagGating.blocked_topic_ids_for(user))
       end
 
       results
@@ -71,16 +74,12 @@ after_initialize do
   end
 
   # --- 3. THE LIST FILTER (TopicQuery) ---
-  module FilterNSFWTopics
+  module FilterGatedTopics
     def default_results(options = {})
       results = super
       return results unless SiteSetting.tag_gating_enabled
-      unless DiscourseTagGating.nsfw_access?(@user)
-        nsfw_tag_id = Tag.where(name: "nsfw").select(:id)
-        nsfw_topic_ids = TopicTag.where(tag_id: nsfw_tag_id).select(:topic_id)
-        blocked_topic_ids =
-          Topic.where(id: nsfw_topic_ids).where.not(user_id: @user&.id).select(:id)
-        results = results.where.not(id: blocked_topic_ids)
+      unless DiscourseTagGating.has_access?(@user)
+        results = results.where.not(id: DiscourseTagGating.blocked_topic_ids_for(@user))
       end
 
       results
@@ -90,33 +89,29 @@ after_initialize do
   # --- 4. THE SEARCH FILTER ---
   Search.advanced_filter(/.*/) do |posts, match|
     next posts unless SiteSetting.tag_gating_enabled
-    unless DiscourseTagGating.nsfw_access?(@guardian&.user)
-      nsfw_tag_id = Tag.where(name: "nsfw").select(:id)
-      nsfw_topic_ids = TopicTag.where(tag_id: nsfw_tag_id).select(:topic_id)
-      blocked_topic_ids =
-        Topic.where(id: nsfw_topic_ids).where.not(user_id: @guardian&.user&.id).select(:id)
-      posts = posts.where.not(topic_id: blocked_topic_ids)
+    unless DiscourseTagGating.has_access?(@guardian&.user)
+      posts = posts.where.not(topic_id: DiscourseTagGating.blocked_topic_ids_for(@guardian&.user))
     end
 
     posts
   end
 
   # --- 5. THE FEATURED TOPICS FILTER (CategoryList) ---
-  module FilterNSFWCategoryList
+  module FilterGatedCategoryList
     def load_topics
       super
       return unless SiteSetting.tag_gating_enabled
       return unless @all_topics
 
-      unless DiscourseTagGating.nsfw_access?(@guardian&.user)
+      unless DiscourseTagGating.has_access?(@guardian&.user)
         user_id = @guardian&.user&.id
         featured_ids = @all_topics.map(&:id)
-        nsfw_tag_id = Tag.where(name: "nsfw").select(:id)
-        nsfw_featured_ids =
-          TopicTag.where(topic_id: featured_ids, tag_id: nsfw_tag_id).pluck(:topic_id).to_set
+        tag_id = Tag.where(name: SiteSetting.tag_gating_tag_name).select(:id)
+        tagged_featured_ids =
+          TopicTag.where(topic_id: featured_ids, tag_id: tag_id).pluck(:topic_id).to_set
         blocked_ids =
           @all_topics
-            .select { |t| nsfw_featured_ids.include?(t.id) && t.user_id != user_id }
+            .select { |t| tagged_featured_ids.include?(t.id) && t.user_id != user_id }
             .map(&:id)
             .to_set
 
@@ -129,8 +124,8 @@ after_initialize do
     end
   end
 
-  CategoryList.prepend FilterNSFWCategoryList
-  TopicQuery.prepend FilterNSFWTopics
-  Post.singleton_class.prepend FilterNSFW
+  CategoryList.prepend FilterGatedCategoryList
+  TopicQuery.prepend FilterGatedTopics
+  Post.singleton_class.prepend FilterGatedPosts
   Guardian.prepend(::DiscourseTagGatingGuardianExtension)
 end
